0e0e90f4f1d728eee39d8661f56cb685a682b6a3
OtaviOuu <conta.otaviou@gmail.com>
2025-02-09 16:35:59 -0300

    Refactor to integrate FastAPI for web scraping functionality and remove legacy code

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from typing import Union
+from spider import ShopiFyHandler
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/home")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/scrape")
+async def scrape_website(
+    request: Request, store_name: str = Form(...), site_url: str = Form(...)
+):
+    shopify = ShopiFyHandler(site_url, store_name)
+
+    products = await shopify.collect()
+
+    return templates.TemplateResponse(
+        "scrape.html", {"request": request, "products": products}
+    )
